b7b4d127497b31ddcd5b5a089cb914911c80234e
fercansen <fercansen@gmail.com>
2019-12-15 02:47:04 +0300

    New Gun Assets Added for FPS
    It includes gun and hand animations, sounds, crosshair, crouching, jumping etc.

---
+++ Assets/ZombieSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour
+{
+
+	public GameObject zombiePrefab;
+
+	private float m_SpawnDelay= 10;
+	private float m_NextSpawnTime;
+	
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Instantiate(zombiePrefab);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ShouldSpawn())
+		{
+			Spawn();
+		}
+	}
+
+	private void Spawn()
+	{
+		m_NextSpawnTime = Time.time + m_SpawnDelay;
+		Instantiate(zombiePrefab, transform.position, transform.rotation);
+	}
+
+	private bool ShouldSpawn()
+	{
+		return Time.time >= m_NextSpawnTime;
+	}
+}
